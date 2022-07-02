23a55636cd82f5e40c721c80c71cec8040b2168f
SuleymanKhasanov <xasanovsuleyman@gmail.com>
2022-07-02 22:35:52 +0500

    Добавил плагины для CSS

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const clear = require("./tusk/clear.js");
 
 const pug = require("./tusk/pug.js");
 
+const css = require("./tusk/css.js");
+
 
 const server = () => {
    browserSync.init({
@@ -20,13 +22,15 @@ const server = () => {
 
 const watcher = () => {
    watch(path.pug.watch, pug).on("all", browserSync.reload);
+   watch(path.css.watch, css).on("all", browserSync.reload);
 };
 
 module.exports.pug = pug;
+module.exports.css = css;
 module.exports.watch = watcher;
 
 module.exports.dev = series(
    clear,
-   pug,
+   parallel(pug, css),
    parallel(watcher, server)
 );
---
+++ tusk/css.js
@@ -0,0 +1,36 @@
+const { src, dest } = require("gulp");
+
+const plumber = require("gulp-plumber");
+
+const notify = require("gulp-notify");
+const concat = require("gulp-concat");
+const cssimport = require("gulp-cssimport");
+const autoprefixer = require('gulp-autoprefixer');
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
+const shorthand = require('gulp-shorthand');
+const path = require("../config/path");
+
+
+
+
+const css = () => {
+   return src(path.css.src, { sourcemaps: true })
+      .pipe(plumber({
+         errorHandler: notify.onError(error => ({
+            title: "css",
+            message: error.message
+
+         }))
+      }))
+      .pipe(concat("main.css"))
+      .pipe(cssimport())
+      .pipe(autoprefixer())
+      .pipe(shorthand())
+      .pipe(dest(path.css.dest, { sourcemaps: true }))
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(csso())
+      .pipe(dest(path.css.dest, { sourcemaps: true }));
+};
+
+module.exports = css;
